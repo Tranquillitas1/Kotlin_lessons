340780fb2f593aaca64a4c2e9c10e56bf8f624f9
Tranquillitas <maksim3000v@yandex.ru>
2023-09-07 14:31:53 +0300

    .

---
+++ src/main/kotlin/lesson1_task5.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var timeInSpaceBySeconds = 6480
+    val timeInSpaceHours = 1
+    val timeInSpaceMinutes = 48
+
+    timeInSpaceBySeconds = 0
+
+    print("0")
+    print(timeInSpaceHours)
+    print(":")
+    print(timeInSpaceMinutes)
+    print(":")
+    print(timeInSpaceBySeconds)
+    print("0")
+
+}
