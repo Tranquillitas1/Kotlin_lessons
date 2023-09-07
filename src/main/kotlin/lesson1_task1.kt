ec6b5c522f4fe42d1390fb52d6cee15ce14382eb
Tranquillitas <maksim3000v@yandex.ru>
2023-09-07 12:39:51 +0300

    .

--- src/main/kotlin/lesson1_task1.kt
+++ src/main/kotlin/lesson1_task1.kt
@@ -1,5 +1,4 @@
 fun main() {
     val countOfOrder = 75
     val purchaseMessage = "Спасибо за покупку"
-
 }
---
+++ src/main/kotlin/lesson1_task2.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val countOfOrder = 75
+    val purchaseMessage = "Спасибо за покупку"
+
+    println(countOfOrder)
+    println(purchaseMessage)
+
+    var countOfworker = 2000
+//    println(countOfworker)
+
+    countOfworker = 1999
+    println(countOfworker)
+}
