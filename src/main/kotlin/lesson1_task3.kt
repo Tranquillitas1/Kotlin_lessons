2865760b5eb4eb83df977ccd9b53243aa43125ab
Tranquillitas <maksim3000v@yandex.ru>
2023-09-07 13:19:35 +0300

    Task 3

---
+++ src/main/kotlin/lesson1_task3.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println(year)
+//    println(hour)
+//    println(minute)
+
+    hour = 10
+    minute = 55
+
+    print(hour)
+    print(":")
+    print(minute)
+}
