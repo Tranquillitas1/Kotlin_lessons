0fb4e80f54191cb6803af3b3bcb5fe09057a3a5f
Tranquillitas <maksim3000v@yandex.ru>
2023-09-08 18:43:13 +0300

    второе задание второго урока

---
+++ src/main/kotlin/lesson2_task2.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val countOfWorker = 50
+    val workersSalary = 30000
+    val countOfNewbee = 30
+    val newbeesSalary = 20000
+
+    val workersCost = countOfWorker * workersSalary
+    val newbeesCost = countOfNewbee * newbeesSalary
+    val totalSalary = workersCost + newbeesCost
+    val averageSalaryForWorker = totalSalary / (countOfWorker + countOfNewbee)
+    println("Расход на постоянных сотрудников составил: $workersCost")
+    println("Расход на стажёров составил: $newbeesCost")
+    println("Средняя оплата сотрудника после устройста стажёров составила: $averageSalaryForWorker")
+}
