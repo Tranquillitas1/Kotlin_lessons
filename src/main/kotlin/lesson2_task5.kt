1df3d7a8f84eb2fbdd72fa58b4e87b2fd891522e
Tranquillitas <maksim3000v@yandex.ru>
2023-09-11 12:55:11 +0300

    пятое задание второго урока

---
+++ src/main/kotlin/lesson2_task5.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val basedСontribution = 70000
+    val interestRate = 16.7
+    val yearsTime = 20
+
+    val finalContribution = basedСontribution * (1 + interestRate/100) * yearsTime
+    println(finalContribution)
+}
