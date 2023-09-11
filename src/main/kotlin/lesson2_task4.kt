9817911e1936347e3ee770f2c54c5992b0e7b0cf
Tranquillitas <maksim3000v@yandex.ru>
2023-09-11 12:32:31 +0300

    четвёртое задание второго урока

---
+++ src/main/kotlin/lesson2_task4.kt
@@ -0,0 +1,15 @@
+import kotlin.time.times
+
+fun main(){
+    val cristal = 7
+    val iron = 11
+
+    val buffValue = 0.2
+
+    val cristalWithBuff = cristal * buffValue
+    val ironWithBuff = iron * buffValue
+    println(cristalWithBuff.toInt())
+    println(ironWithBuff.toInt())
+
+
+}
