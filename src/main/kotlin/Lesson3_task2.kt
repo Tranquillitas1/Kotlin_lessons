302196dacca40a40c4683b888d090b7a48b070b3
Tranquillitas <maksim3000v@yandex.ru>
2023-09-12 21:13:33 +0300

    второе задание третьего урока

---
+++ src/main/kotlin/Lesson3_task2.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val pastUserName = "Татьяна"
+    val pastUserSecondName = "Андреева"
+    val pastUserSurname = "Сергеевна"
+    val pastUserAge = 20
+
+    val newUserName = "Татьяна"
+    val newUserSecondName = "Сидорова"
+    val newUserSurname = "Сергеевна"
+    val newUserAge = 22
+
+    println("ФИО: $pastUserSecondName $pastUserName $pastUserSurname")
+    println("Возраст: $pastUserAge")
+
+    println("ФИО: $newUserSecondName $newUserName $newUserSurname")
+    println("Возраст: $newUserAge")
+}
