6eca8151f4702e2e5b2542b428a6bd03255f188a
Tranquillitas <maksim3000v@yandex.ru>
2023-09-13 12:19:49 +0300

    первое задание третьего урока

---
+++ src/main/kotlin/lesson3_task1.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val greetingsDay = "Добрый день"
+    val greetingsEvening = "Добрый вечер"
+
+    val userName = "Максим"
+
+    println("$greetingsDay, $userName!")
+    println("$greetingsEvening, $userName!")
+}
